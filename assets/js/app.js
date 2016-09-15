d39e7257962b5cb6740cfe8b592c0a62a0d17aff
SarahReyes <siwreyes@gmail.com>
2016-09-15 15:55:17 -0500

    click buttons in dashboard, updated style, ajax call for category ids

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,78 +1,137 @@
-/*
-----Authenticating requests
-	Once you have an OAuth token, you must include it on all requests - every request to the OAuth API must be authenticated, no anonymous access is allowed.
-	
-	You have two options to pass the token: using the authentication header (preferred) or as a query string parameter
-	
-	Authorization header:
-	Just include an Authorization header with the value Bearer MYTOKEN:
-	
-	Authorization: Bearer 5MWI565BZ5QH5UNU3XV7
-	Query string parameter¶
-	Include the token on the end of the URL as the token parameter:
-	https://www.eventbriteapi.com/v3/users/me/?token=5MWI565BZ5QH5UNU3XV7
+// Firebase database config info
+var config = {
+    apiKey: "AIzaSyDaPvYALiV1qQwyDxIrIfkwCUerTu_uSiA",
+    authDomain: "firstproject-b5d04.firebaseapp.com",
+    databaseURL: "https://firstproject-b5d04.firebaseio.com",
+    storageBucket: "firstproject-b5d04.appspot.com",
+ };
 
-----Parameters
-	The API accepts parameters in several ways:
+// Initialize Firebase for use in app
+firebase.initializeApp(config);
+var database = firebase.database();
+const auth = firebase.auth();
 
-	As query string parameters (commonly called “GET parameters”). These are just simple string keys with urlencoded values.
-	As form-urlencoded POST body data. Keys here are usually dotted (for example event.name.html - you should include the dots as well.
-	As JSON-format POST body data. This is accepted wherever form-encoded POST data is; it’s a series of nested objects with the keys matching the dotted paths of the variable names.
-	For example, submitting this as a urlencoded POST body:
-	
-	event.name.html=Awesome%20Event&event.listed=true
-	is the same as submitting this JSON body (notice how the keys match):
-	
-	{
-	    "event": {
-	        "name": {
-	            "html": "Awesome Event"
-	        },
-	        "listed": true
-	    }
+// testing Sarah's object for each category id
+var testSarahsObject = {};
+
+// Object for storing event properties and methods specific to any event actions
+var eventObj = {
+
+	/**
+	 * Run the ajax call to the Eventbrite api
+	 * @param {object} dataObj Contains parameters as an object
+	 * @return N/A
+	 */
+	ajaxCall: function(dataObj) {
+		var url = "https://www.eventbriteapi.com/v3/events/search/?token=IVKXSGGHO6MZSHMF5QZZ&location.address=austin&location.within=10mi";
+
+		$.ajax({
+			url: url,
+			method: "GET",
+		}).done(function(response) {
+
+			console.log("Response: " + response.events[1]);
+
+			// Loop through each event item
+			response.events.forEach(function(item, index, arr) {
+
+				// Set easy access to name
+				var name = item.name.text;
+
+				// Set easy access to date. Format it using moment.js plugin
+				var date = moment(item.start.local).format('MMMM Do YYYY');
+
+				// Set easy access to event description
+				var desc = item.description.text;
+				var shorDesc = "";
+				var fullDesc = "";
+
+				// If there is no item description, set default message. Save long description for modal dropdown
+				if (item.description.text != null) {
+					longDesc  = desc;
+					shortDesc = desc.slice(0, 100) + "...";
+				} else {
+					longDesc = shortDesc = "No description available.";
+				}
+
+				// Build string of html content, filling in variable content with response items. fullDesc will be used for modal dropdown of full description
+				let html = '<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 event-box">' +
+		                  		'<div class="panel event-content text-center" id="event' + index + '">' +
+		                    		'<h3 class="event-name" data-name="' + name + '">' + name + '</h3>' +
+		                    		'<p class="event-date" data-date="' + date + '">' + date + '</p>' +
+		                    		'<p class="event-desc" data-desc="' + longDesc + '">' + shortDesc + '</p>' +
+		                  		'</div>' +
+		                  		'<button type="button" class="btn btn-lg btn-block fav-button">Add Favorite</button>' +
+		        		  '</div>';
+
+		        // Append new event block to div.main
+		        $(".main").append(html);
+			});
+		});
 	}
-	You cannot mix and match JSON-style inputs and GET/POST style; if you pass a JSON body (or any request body with a Content-Type header set to application/json), all GET and POST parameters will be ignored.
+}
 
-----Getting Events from Eventrbrite
-	** Note: go to the link below to see an example of event search parameters available:
-	https://www.eventbrite.com/developer/v3/endpoints/events/#ebapi-get-events-search
+$(document).ready(function() {
 
-	Notable items from documentation above:
-		-start_date.range_start
-		-start_date.range_end
-		-location.address
-		-location.within
-		-categories
+	// Get the events on initial page load (50 of the first "All" events from the api)
+	eventObj.ajaxCall();
 
+	// If the user clicks on an event box, show modal with event info (giving greater detail of description etc)
+	// Had to start with div.main parent, then narrow down, due to DOM being updated dynamically with events after initial page load
+	$('.main').on('click', 'div.event-content', function() {
+		console.log("this: " + this);
 
-*/
-// Eventbrite token: D5JWVCEPRXACNC6KHG
-// Client Secret: WSRX6O2WIGCMZXHISX35DBDPI5X3IXJWNLDFDI7YRUUPFGQ3E3
-// Personal OAuth token: 5MWI565BZ5QH5UNU3XV7
-// Anonymout OAuth token: FE7TB42RH5HP5CBOINFE
+		// Get the parent element for reference
+		var parentId = "#" + $(this).attr("id");
+		console.log("parentId: " + parentId);
 
-// Get values from the form inputs
-var city = $('#search-city').val();
-var category = $('#search-category').val();
-var date1 = $('#search-date1').val();
-var date2 = $('#search-date2').val();
-console.log("City: " + city + " Category: " + category + " Date 1: " + date1 + " Date 2: " + date2);
-return false;
+		// Fill in the div.eventModal elements with updated content
+		var data;
 
-// Get the parent element which content will be prepended to
-//var parent = $("div#ajax-results");
+		// Get event name
+		data = $(parentId + ' h3').data("name");
+		$('#modal-event-name').text(data);
 
-// Establish query URL, meeting Eventbrite API requirements
-var queryUrl = "https://www.eventbriteapi.com/v3/events/?token=5MWI565BZ5QH5UNU3XV7&location= " + city + "&start_date.range_start=" + date1 + "&start_date.range_end=" + date2 + ";
-console.log("queryUrl: " + queryUrl);
+		// Get event date
+		data = $(parentId + ' p.event-date').data("date");
+		$('#modal-event-date').text(data);
 
-// AJAX call to get data
-$.ajax({
-	url: queryUrl, 
-	method: "GET"
-}).done(function(returnData) {
-	console.log(returnData);
+		// Get event description
+		data = $(parentId + ' p.event-desc').data("desc");
+		$('#modal-event-desc').text(data);
 
-	// Throw stuff into main img container
-	$('div#gif-content').html(stuff);
-})
+		// Now show the modal
+		$('.eventModal').modal("show");
+	});
+
+	// clickable event categories to narrow user search (Sarah 09/14)
+	// on click function to display only the events within that specific category
+	$('.click-option').on('click', function(){
+		// assign the data from the button to eventInfo
+		var eventInfo = $(this).data();
+		// testing that we're getting the id back when clicking
+		console.log(eventInfo.id);
+		// url string concatinated with the category id
+		var queryURL = "https://www.eventbriteapi.com/v3/events/search/?categories=" + eventInfo.id + "&token=IVKXSGGHO6MZSHMF5QZZ";
+		// ajax call which will work for each individual catgetory id
+		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			var results = response.data;
+			console.log(response);
+			// grab the values that you want to display from the object
+			console.log(response.events[0].description);
+			// populate the chosen values in the div
+		});
+	});
+
+});
+// q = events with keyword
+// start_date.range_start
+// start_date.range_end
+//location.address
+// location.within
+// categories
+//response.events[i].description.text
+//response.events[i].end.local
+// "".name.text
+// "" start.local
+// ""
