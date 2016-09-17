00b82667952dc56c3b8019f89a5253437eea62d6
Matt Holland <mtholland10@gmail.com>
2016-09-17 13:34:47 -0500

    Update main home page, add db testing files

--- assets/js/db-testing.js
+++ assets/js/db-testing.js
@@ -1,107 +1,130 @@
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyA9gw81RM2br5S9X55E0G6ZGjJMo1oDVRs",
-  authDomain: "eventi-testing-db.firebaseapp.com",
-  databaseURL: "https://eventi-testing-db.firebaseio.com",
-  storageBucket: "eventi-testing-db.appspot.com",
-  messagingSenderId: "393804341426"
-};
-firebase.initializeApp(config);
-
-// Establish easy access to db object
-var db = firebase.database();
-
 $(document).ready(function() {
 
-	// Include the backstretch plugin to make the background images fully responsive and rotating
-	$.backstretch(
-		[
-  	      "assets/img/chicago.jpg",
-  	      "assets/img/flaming-lips.jpg",
-  	      "assets/img/concert-image-02.jpg"
-  		], 
-  		// Duration == pause time, fade == transition time
-  		{
-  			duration: 6000,
-  			fade: 1400
-  		}
-  	);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9gw81RM2br5S9X55E0G6ZGjJMo1oDVRs",
+    authDomain: "eventi-testing-db.firebaseapp.com",
+    databaseURL: "https://eventi-testing-db.firebaseio.com",
+    storageBucket: "eventi-testing-db.appspot.com",
+    messagingSenderId: "393804341426"
+  };
+  firebase.initializeApp(config);
+  
+  // Establish easy access to db object
+  var db = firebase.database();
+  
+  // Establish database references
+  var usersRef = db.ref("users");
+  var nameRef = db.ref("users/name")
+  var musicRef = db.ref("users").child("favCategories");
+  
+  // Retrieve every user's information on page load
+  usersRef.on('child_added', function(snapshot) {
+    var content = "<p> User key: " + snapshot.key + " User name: " + snapshot.val().name + " User email: " + 
+    snapshot.val().email + " User favCategories: " + snapshot.val().favCategories + "</p>";
+    $('.drunk-ass-div').append(content);
+  });
 
-  	// Include the datepicker, from jQuery UI library
-  	$("#search-date1").datepicker();
-  	$("#search-date2").datepicker();
+  // Retrieve just the first two users' information
+  usersRef.limitToFirst(2).on('child_added', function(snapshot) {
+    var content = "<p> User key: " + snapshot.key + " User name: " + snapshot.val().name + " User email: " + 
+    snapshot.val().email + " User favCategories: " + snapshot.val().favCategories + "</p>";
+    $('.first-user').append(content);
+  });
 
-    // Produce event result content when user submits main page form
-    /*
+  // Retrieve all favorites for user 757827487
+  usersRef.child('757827487').child("favCategories").on('child_added', function(snapshot) {
+    var faves = snapshot.key + "<br>";
+    $('.faves').append(faves);
+  });
+
+  // Retrieve all music events
+  usersRef.on('child_added', function(snapshot) {
+    var username = snapshot.val().name;
+    var musicEvent = snapshot.val().favCategories.music;
+    var content = "<p> User: " + username + "<br>";
+    musicEvent.forEach(function(item, index, arr) {
+      content += "<span> Event #" + index + ": " + item.desc + "</span><br>";
+    });
+    content += "</p>";
+    $('.music').append(content);
+  });
+
+    /* This is the actual starter database (may have added/altered since) 
     db.ref().set({
       users: {
         4738473827: {
           name: "Jeff",
           email: "mt@m.com",
           favCategories: {
-            music: {
-              event1: {
+            music: [
+              {
                 name: "Blades of Glory",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+
+              {
                 name: "Blades of Jazz",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Nast",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
           
-            arts: {
-              event1: {
+            arts: [
+              {
                 name: "Blades of Bitches",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+
+              {
                 name: "Blades of Fayyyce",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Dodge",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
 
-            sports: {
-              event1: {
+            sports: [
+              {
                 name: "Blades of Soccer",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Golf",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Pinball",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            }
+            ]
           } // favCategories
         },
 
@@ -109,68 +132,74 @@ $(document).ready(function() {
           name: "Michael",
           email: "m@h.com",
           favCategories: {
-            music: {
-              event1: {
+            music: [
+              {
                 name: "Blades of 1",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of 2",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of 3",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
   
-            arts: {
-              event1: {
+            arts: [
+              {
                 name: "Blades of 1",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of 2",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of 3",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
 
-            sports: {
-              event1: {
+            sports: [
+              {
                 name: "Blades of 1",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of 2",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of 3",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            }
+            ]
           } // favCategories
         },
 
@@ -178,145 +207,151 @@ $(document).ready(function() {
           name: "Harry",
           email: "m@harry.com",
           favCategories: {
-            music: {
-              event1: {
+            music: [
+              {
                 name: "Blades of Glory",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Jazz",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Nast",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
     
-            arts: {
-              event1: {
+            arts: [
+              {
                 name: "Blades of Bitches",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Fayyyce",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Dodge",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
 
-            sports: {
-              event1: {
+            sports: [
+              {
                 name: "Blades of Soccer",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Golf",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Pinball",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            }
+            ]
           } // favCategories
         },
 
         111627626: {
           name: "Isaac",
           email: "m@isaac.com",
-          favCategores: {
-            music: {
-              event1: {
+          favCategories: {
+            music: [
+              {
                 name: "Blades of Glory",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+
+              {
                 name: "Blades of Jazz",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+
+              {
                 name: "Blades of Nast",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
     
-            arts: {
-              event1: {
+            arts: [
+              {
                 name: "Blades of Bitches",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Fayyyce",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Dodge",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            },
+            ],
 
-            sports: {
-              event1: {
+            sports: [
+              {
                 name: "Blades of Soccer",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event2: {
+              
+              {
                 name: "Blades of Golf",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               },
-              event3: {
+              
+              {
                 name: "Blades of Pinball",
                 date: "07/07/16",
                 desc: "Go down in blades of glory, man!",
                 location: "2025 E 7th St"
               }
-            }
+            ]
           } // favCategories
         } // player unique id
       } // users
-    });*/
-    
-    // Db testing
-    var ref = db.ref("users");
-    ref.on('child_added', function(snapshot) {
-      console.log("User key: " + snapshot.key + " User name: " + snapshot.val().name + " User email: " + snapshot.val().email + " User favCategories: " + snapshot.val().favCategories);
-    });
-});
+    }); // set function */
+}); // $(document).ready() {}
