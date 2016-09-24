8e78bfd4692a7f6353cdab43ffa409a52c0408a8
Matt Holland <mtholland10@gmail.com>
2016-09-24 07:39:41 -0500

    I'm tired...

--- assets/js/app.js
+++ assets/js/app.js
@@ -67,18 +67,18 @@ firebase.auth().getRedirectResult().then(function(result) {
   if (result.credential) {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
-    console.log(token);
-    console.log(result);
+    console.log("token: " + token);
+    console.log("result: " + result);
     user = result.user;
-    console.log(user.displayName);
+    console.log("diplay name: " + user.displayName);
     userName = user.displayName;
     userEmail = user.email;
-    console.log(user.email);
-    console.log(user.photoURL);
+    console.log("email: " + user.email);
+    console.log("photo url: " + user.photoURL);
     userImg = user.photoURL;
     currentUserProfile = firebase.auth().currentUser;
     uidUser = firebase.auth().currentUser.uid;
-    (console.log(uidUser));
+    console.log("uidUser: " + uidUser);
     currentUserRef = database.ref('Users/'+uidUser);
     currentUserRef.update({
          name: userName,
